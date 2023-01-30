5445f54490a81bc36891347f479c8cdfc0d6e843
EnzoJF6 <123915333+EnzoJF6@users.noreply.github.com>
2023-01-29 23:39:02 -0300

    initial commit

---
+++ java_proyecto.js
@@ -0,0 +1,10 @@
+
+function m1(){
+    document.getElementById('b1').style.display="block";
+    document.getElementById('b2').style.display="none";
+  };
+  
+  function m2(){
+    document.getElementById('b1').style.display="none";
+    document.getElementById('b2').style.display="block";
+  };
