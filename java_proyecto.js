1b0ec954991d53675248846c8c4e68915123e9f2
EnzoJF6 <123915333+EnzoJF6@users.noreply.github.com>
2023-03-26 12:01:15 -0300

    Update java_proyecto.js

--- java_proyecto.js
+++ java_proyecto.js
@@ -8,3 +8,12 @@ function m1(){
     document.getElementById('b1').style.display="none";
     document.getElementById('b2').style.display="block";
   };
+function m3(){
+    document.getElementById('b3').style.display="block";
+    document.getElementById('b4').style.display="none";
+  };
+  
+  function m4(){
+    document.getElementById('b3').style.display="none";
+    document.getElementById('b4').style.display="block";
+  };
